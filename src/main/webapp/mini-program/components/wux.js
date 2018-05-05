67fb5dc520d08b221123d319405f13d4f1d9eafe
Raj.Huang <hsm19950709@gmail.com>
2018-05-05 16:07:09 +0800

    删除无用js

--- src/main/webapp/mini-program/components/wux.js
+++
@@ -1,9 +0,0 @@
-import $wuxButton from 'button/button'
-import $wuxToast from 'toast/toast'
-import $wuxToptips from 'toptips/toptips'
-
-export {
-    $wuxButton, 
-    $wuxToast,
-    $wuxToptips,
-}
